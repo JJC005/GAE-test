74575aa550be2f1733fb270eb3cb795724b83396
JJC005 <46185812+JJC005@users.noreply.github.com>
2018-12-27 03:57:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python37_app]
 from flask import Flask
 
 
@@ -20,16 +5,14 @@ from flask import Flask
 # called `app` in `main.py`.
 app = Flask(__name__)
 
-
 @app.route('/')
+#START MY CODE
 def hello():
-    """Return a friendly HTTP greeting."""
     return 'Oh please Lord, please let this message appear. If it does, then I have no idea how...'
 
-
+#END MY CODE
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python37_app]
