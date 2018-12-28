8eb22e4047424fc2277498fb42adb02260156eba
JJC005 <46185812+JJC005@users.noreply.github.com>
2018-12-28 01:57:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
 app = Flask(__name__)
-@app.route('/')
 
 #START MY CODE*****************************************************************************************************************************
+# Index
+@app.route('/')
+def index():
+    return render_template('home.html')
 
-def hello():
-    return 'Only Joey should be able to see this'
+#Will complete for login page
+@app.route('/login')
+def login():
+    return 'This is where the login page goes'
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
 
 #END MY CODE*******************************************************************************************************************************
 
