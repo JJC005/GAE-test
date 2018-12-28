7d8f9b1ccaa477b66c9e5b50a8639306462bf04b
JJC005 <46185812+JJC005@users.noreply.github.com>
2018-12-28 02:01:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
-from functools import wraps
+#from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+#from passlib.hash import sha256_crypt
+#from functools import wraps
 app = Flask(__name__)
 
 #START MY CODE*****************************************************************************************************************************
@@ -16,23 +16,23 @@ def login():
     return 'This is where the login page goes'
 
 # Check if user logged in
-def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Unauthorized, Please login', 'danger')
-            return redirect(url_for('login'))
-    return wrap
+#def is_logged_in(f):
+   # @wraps(f)
+    #def wrap(*args, **kwargs):
+     #   if 'logged_in' in session:
+      #      return f(*args, **kwargs)
+       # else:
+        #    flash('Unauthorized, Please login', 'danger')
+         #   return redirect(url_for('login'))
+   # return wrap
 
 # Logout
-@app.route('/logout')
-@is_logged_in
-def logout():
-    session.clear()
-    flash('You are now logged out', 'success')
-    return redirect(url_for('login'))
+#@app.route('/logout')
+#@is_logged_in
+#def logout():
+ #   session.clear()
+  #  flash('You are now logged out', 'success')
+   # return redirect(url_for('login'))
 
 #END MY CODE*******************************************************************************************************************************
 
